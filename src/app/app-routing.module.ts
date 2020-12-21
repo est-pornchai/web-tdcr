383e955b22a4092301142a8eb6b1f071bbc21194
Est <pornchai.tdcr@gmail.com>
2020-12-21 16:37:47 +0700

    ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { CompanyComponent } from '../app/company/company.component';
 import { AppComponent } from '../app/app.component';
 
 const routes: Routes = [
-{path:"", redirectTo: '/home', pathMatch: 'full'},
-{path:"home", component:AppComponent},
 {path:"company", component:CompanyComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { CompanyComponent } from './company/company.component';
 import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AboutComponent } from './about/about.component';
     HomeComponent,
     HeaderComponent,
     CompanyComponent,
-    AboutComponent
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
