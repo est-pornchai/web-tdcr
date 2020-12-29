3968c388a66e8ff4ba5c20099126d15f1a7e27cb
Est <pornchai.tdcr@gmail.com>
2020-12-29 09:25:28 +0700

    ...

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -9,19 +9,9 @@ import { ViewChild ,Input} from '@angular/core';
   styleUrls: ['./company.component.css']
 })
 export class CompanyComponent {
-  @ViewChild("agGrid",{static: false})agGrid : AgGridAngular;
+  
 
-  columnDefs = [
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price'}
-];
-
-rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
-];
+ 
   constructor() { }
 
   ngOnInit(): void {
